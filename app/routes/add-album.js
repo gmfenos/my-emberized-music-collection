228b8fcde9d435165f250fcf3f32d4cd8865a7f9
Gabriel Fenos <gabriel.fenos@grupoesfera.com.ar>
2019-11-09 18:46:56 -0300

    Creates new album on navigating to add-album

--- app/routes/add-album.js
+++ app/routes/add-album.js
@@ -1,4 +1,8 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+    beforeModel() {
+        let controller = this.controllerFor('add-album');
+        controller.set('newAlbum', this.get('store').createRecord('album'));
+    }
 });
