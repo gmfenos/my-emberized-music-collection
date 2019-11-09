ee2f606f8d733f771a022268bcb7ad1653189e27
Gabriel Fenos <gabriel.fenos@grupoesfera.com.ar>
2019-11-09 18:47:20 -0300

    Adds test to verify my-collection looks for albums

--- tests/unit/controllers/my-collection-test.js
+++ tests/unit/controllers/my-collection-test.js
@@ -4,9 +4,21 @@ import { setupTest } from 'ember-qunit';
 module('Unit | Controller | my-collection', function(hooks) {
   setupTest(hooks);
 
-  // Replace this with your real tests.
   test('it exists', function(assert) {
     let controller = this.owner.lookup('controller:my-collection');
     assert.ok(controller);
+  });
+
+  test('gets albums from store', function(assert) {
+    assert.expect(1);
+
+    let controller = this.owner.lookup('controller:my-collection');
+    controller.set('store', {
+      findAll(what) {
+        assert.equal(what, 'album', 'looking for all albums');
+      }
+    });
+
+    controller.send('getAlbums');
   });
 });
