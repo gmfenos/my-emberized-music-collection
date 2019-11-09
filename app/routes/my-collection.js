fe302f785c4f3c806b66e8ea6a5ad85f91b5e90b
Gabriel Fenos <gabriel.fenos@grupoesfera.com.ar>
2019-11-09 18:19:13 -0300

    Shows collection of albums

--- app/routes/my-collection.js
+++ app/routes/my-collection.js
@@ -1,4 +1,8 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+    beforeModel() {
+        let controller = this.controllerFor('my-collection');
+        controller.send('getAlbums');
+    }
 });
