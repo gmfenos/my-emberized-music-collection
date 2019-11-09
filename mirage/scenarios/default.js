ba705351d312c8e1a8e6f3e7427b4d3b00b7ef78
Gabriel Fenos <gabriel.fenos@grupoesfera.com.ar>
2019-11-09 18:32:39 -0300

    Adds basic styles for collection table

--- mirage/scenarios/default.js
+++ mirage/scenarios/default.js
@@ -4,7 +4,7 @@ export default function(server) {
     artistName: 'Dream Theater',
     albumName: 'Six degrees of inner turbulence',
     genres: 'Progressive Metal',
-    albumCoverUrl: 'https://en.wikipedia.org/wiki/Six_Degrees_of_Inner_Turbulence#/media/File:Dream_Theater_-_Six_Degrees_of_Inner_Turbulence.jpg',
+    albumCoverUrl: 'https://upload.wikimedia.org/wikipedia/en/3/3d/Dream_Theater_-_Six_Degrees_of_Inner_Turbulence.jpg',
     yearOfRelease: 2002,
     spotifyLink: 'https://open.spotify.com/album/4INFVEhhhnLiqNR4MVPaX8'
   });
@@ -12,7 +12,7 @@ export default function(server) {
     artistName: 'Led Zeppelin',
     albumName: 'The song remains the same',
     genres: 'Rock',
-    albumCoverUrl: 'https://en.wikipedia.org/wiki/The_Song_Remains_the_Same_(album)#/media/File:Led_Zeppelin_-_The_Song_Remains_the_Same.jpg',
+    albumCoverUrl: 'https://upload.wikimedia.org/wikipedia/en/b/b4/Led_Zeppelin_-_The_Song_Remains_the_Same.jpg',
     yearOfRelease: 1976,
     spotifyLink: 'https://open.spotify.com/album/0ui4S0TZghkf1d1Wz0oWpk'
   });
